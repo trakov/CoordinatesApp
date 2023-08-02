0dd1004a186ce762722cab720d4b9334f7fc71ea
Timur Rakov <ermorder@gmail.com>
2023-08-02 13:31:17 +0300

    Initial Commit

---
+++ Coordinates/CoordinatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoordinatesApp.swift
+//  Coordinates
+//
+//  Created by Timur Rakov on 02.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct CoordinatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
