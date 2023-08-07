92c0e0a25327c358fae5626c002bfc5257c44760
Timur Rakov <ermorder@gmail.com>
2023-08-07 14:14:04 +0300

    For test purposes

--- Coordinates/CoordinatesApp.swift
+++ Coordinates/CoordinatesApp.swift
@@ -5,6 +5,15 @@ struct CoordinatesApp: App {
     var body: some Scene {
         WindowGroup {
             MainView()
+//            DetailView(points: [
+//                Point(x: -2.0, y: -2.0),
+//                Point(x: -1.0, y: 2.0),
+//                Point(x: 0.0, y: -3.0),
+//                Point(x: 1.0, y: 4.0),
+//                Point(x: 2.0, y: -2.0),
+//                Point(x: 3.0, y: 5.0),
+//                Point(x: 4.0, y: -3.0)
+//            ])
         }
     }
 }
